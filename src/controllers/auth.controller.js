96f7097464281586d57f3f2ed8e3dee3f59990da
Michal Gnacinski <michal.gnacinski2006@gmail.com>
2026-06-07 09:48:12 +0200

    Fully authentication with jwt + postman tests

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,43 @@
+import { formatValidationError } from "#utils/format.js";
+import { signupSchema } from "#validations/auth.validations.js";
+import logger from "#config/logger.js";
+import { createUser } from "#servies/auth.service.js";
+import { jwttoken } from "#utils/jwt.js";
+import { cookies } from "#utils/cookies.js";
+
+export const signup = async (req, res, next) => {
+    try{
+        const validationResult = signupSchema.safeParse(req.body);
+
+        if(!validationResult.success) {
+            return res.status(400).json({
+                error: 'Validation failed',
+                details: formatValidationError(validationResult.error)
+            })
+        }
+
+        const { name, email, password, role } = validationResult.data;
+
+        const user = await createUser( {name, email, password, role})
+
+        const token = jwttoken.sign({ id: user.id, email: user.email, role: user.role});
+
+        cookies.set(res, 'token', token);
+
+        logger.info('User  registred succesfuly: ${email}');
+        res.status(201).json({
+            message: 'User registred',
+            user: {
+                id: user.id, name: user.name, email: user.email, role: user.role
+            }
+        })
+    } catch (e) {
+        logger.error('signup error', e);
+
+        if(e.message == 'USer with this email already exists') {
+            return res.status(409).json({ error: 'Email already exists' });
+        }
+
+        next(e);
+    }
+}
--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-import app from './app.js';
+import app from './app.js'
 
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('We are listening on http://${PORT}');
+  console.log(`We are listening on http://localhost:${PORT}`);
 });
